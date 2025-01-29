6251681b08e3063fe4f152b31e2467f81d7186d7
Debabrata Halder <108052492+DebabrataHalder@users.noreply.github.com>
2025-01-29 10:50:59 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -144,7 +144,7 @@ with st.sidebar:
         "Select Model", 
         # options=["llama-3.1-70b-versatile", "mixtral-8x7b-32768","gemma2-9b-it"],
         # index=0
-        options=["llama-3.1-70b-versatile"],
+        options=["llama-3.3-70b-versatile","gemma2-9b-it"],
         index=0
     )
     
